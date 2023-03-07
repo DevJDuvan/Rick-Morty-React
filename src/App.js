cd292a773198903a9bf062b6f29512eba3fdef7a
jose <jose.carranza@correounivalle.edu.co>
2023-03-06 20:49:23 -0500

    Components &&  pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 // importamos los estilos de boostrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import "./App.scss";
 import React, { useState, useEffect } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+import Filter from "./components/Filter/Filter";
+import Card from "./components/Card/Card";
 
 
 function App() {
     return (
       <Router>
-       
+        <div className="App">
+          <Navbar />
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
@@ -22,11 +27,49 @@ function App() {
 
 
   const Home = () => {
-   
+    let [pageNumber, updatePageNumber] = useState(1);
+    let [search, setSearch] = useState("");
+    let [status, updateStatus] = useState("");
+    let [gender, updateGender] = useState("");
+    let [species, updateSpecies] = useState("");
+    let [fetchedData, updateFetchedData] = useState([]);
+    let { info, results } = fetchedData;
+    let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+      
+// consumo de api
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
     return (
       <div className="App">
-        <h1 className="text-center mb-3">Palabra clave</h1>
-     
+        <h1 className="text-center mb-3 Title">Capitulo</h1>
+        <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+        
+      <div className="container">
+        <div className="row">
+        <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
+ <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card page="/" results={results} />
+            </div>
+          </div>
+          </div>
+          </div>
+          <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
       </div>
     );
   };
